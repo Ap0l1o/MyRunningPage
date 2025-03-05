1ca13600127a6f8145ce87e89a0bc42610f839bd
ap0l1o <15463990+ap0l1o@user.noreply.gitee.com>
2025-03-05 22:52:13 +0800

    Initial commit from gatsby

---
+++ gatsby-config.js
@@ -0,0 +1,10 @@
+/**
+ * @type {import('gatsby').GatsbyConfig}
+ */
+module.exports = {
+  siteMetadata: {
+    title: `my-running-site`,
+    siteUrl: `https://www.yourdomain.tld`,
+  },
+  plugins: [],
+}
